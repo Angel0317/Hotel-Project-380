306ed151d88b19600bf27b44de2ec26f7da5cd80
wesleyccox291 <77998331+wesleyccox291@users.noreply.github.com>
2022-08-15 17:55:26 -0700

    Added Comments
    Added Comment marker for where the connection string needs to be changed

--- Hotel Project 380/Edit.cs
+++ Hotel Project 380/Edit.cs
@@ -17,6 +17,7 @@ namespace Hotel_Project_380
          * Connect Sql Server data for all relevant reservation information
          */
 
+        //Change SQL connection to HotelDB.mdf
         SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Student.IT-STULOAN-714.004\Documents\GitHub\Hotel-Project-380\HotelDB.mdf;Integrated Security=True;Connect Timeout=30");
         public void populate()
         {
